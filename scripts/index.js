da82f36773ace571240268224a6de2c44d1c05f6
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2024-11-14 00:23:30 -0300

    feat: clearSelect, home.js try catch, setMap, requests.http

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,34 @@
   const $startSelect = document.getElementById("start_select");
   const $indexForm = document.getElementById("index-form");
 
-  let jsonData;
+  let jsonData; // variável armazena os dados do JSON -> ele começa vazio
 
-  async function RenderOptionsInSelect(select) {
-    const $select = document.getElementById(select);
+  // Essa função irá limpar as opções do select que será passado no select_name
+  function clear_select(select_name) {
+    const select = document.getElementById(select_name);
+    for (let position = select.options.length - 1; position > 0; position -= 1) {
+      select.remove(position);
+    }
+  }
 
-    if (!jsonData) jsonData = await LoadJSON(`./database/data.json`);
+  async function RenderOptionsInSelect(select_name) {
+    // Método reutilizável para inserir opções dentro de um seletor
+    const $select = document.getElementById(select_name); // armazena o seletor em uma váriavel através do nome passado
 
+    if (!jsonData) {
+      // se o jsonData estiver vazio, ele vai carregar os dados do JSON a partir do código abaixo
+      jsonData = await LoadJSON(`./database/data.json`); // carregar os dados do JSON e salvar na variável
+    }
+
+    // se o jsonData estiver cheio, ele vai limpar o seletor e  renderizar as opções dentro dele
     if (jsonData) {
-      jsonData[select].forEach((item) => {
-        const $option = document.createElement("option");
-        $option.value = item.value;
-        $option.innerHTML = item.label;
-        $select.appendChild($option);
+      clear_select(select_name); // limpando as opções do seletor
+      jsonData[select_name].forEach((item) => {
+        const $option = document.createElement("option"); // criar um html OPTION
+        $option.value = item.value; // o valor do OPTION é igual o valor do ITEM do ARRAY DO JSON
+        $option.innerHTML = item.label; // o texto do OPTION é igual o texto do ITEM do ARRAY DO JSON
+
+        $select.appendChild($option); // adicionar o OPTION criado dentro do seletor
       });
     }
   }
